6926eb8de039188478590b912f765573d2a16b39
windows_on_wsl <otomijuf.004@gmail.com>
2023-03-26 19:02:58 +0900

    fix

--- generate.py
+++ generate.py
@@ -109,7 +109,8 @@ def main(
             )
         s = generation_output.sequences[0]
         output = tokenizer.decode(s)
-        return output.split("### Response:")[1].strip()
+        # return output.split("### Response:")[1].strip()
+        return output.split("### 応答:")[1].strip()
 
     gr.Interface(
         fn=evaluate,
