8b637e51975e85578d0f30df825de20f5db21551
windows_on_wsl <otomijuf.004@gmail.com>
2023-06-30 02:42:05 +0900

    add for xgen

--- finetune_xgen.py
+++ finetune_xgen.py
@@ -293,7 +293,7 @@ def train(
             run_name=wandb_run_name if use_wandb else None,
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
-            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=False
         ),
     )
     model.config.use_cache = False
