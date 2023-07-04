1149a470a7ca2822fdb815e2523234bd8d84013d
windows_on_wsl <otomijuf.004@gmail.com>
2023-07-05 01:00:35 +0900

    fix

--- finetune_xgen_conv_data.py
+++ finetune_xgen_conv_data.py
@@ -252,6 +252,7 @@ def train(
         train_val = data["train"].train_test_split(
             test_size=val_set_size, shuffle=True, seed=42
         )
+        print('train_val', train_val)
         train_data = generate_and_tokenize_prompt(train_val["train"].shuffle())
         print("train_data", train_data[0])
         val_data = generate_and_tokenize_prompt(train_val["test"].shuffle())
