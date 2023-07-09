a88b3d63e9028b4ad594d6896b4f17da57fe6ff1
windows_on_wsl <otomijuf.004@gmail.com>
2023-07-09 16:12:27 +0900

    fix

--- finetune_xgen_conv_data.py
+++ finetune_xgen_conv_data.py
@@ -136,7 +136,7 @@ class DataCollatorForSeq2SeqDebug:
             return_tensors=return_tensors,
         )
         
-        print('batch input_ids, labels: ', len(feature['input_ids']), len(feature['labels']))        
+        print('batch input_ids, labels: ', len(features['input_ids']), len(features['labels']))        
         # prepare decoder_input_ids
         if (
             labels is not None
