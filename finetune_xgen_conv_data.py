e36219e0e825a97c472c7a60e3930c45c0c1cc6f
windows_on_wsl <otomijuf.004@gmail.com>
2023-07-11 00:43:46 +0900

    fix special token

--- finetune_xgen_conv_data.py
+++ finetune_xgen_conv_data.py
@@ -55,120 +55,6 @@ from transformers.utils import PaddingStrategy
 from transformers.tokenization_utils import PreTrainedTokenizerBase
 import numpy as np
 
-@dataclass
-class DataCollatorForSeq2SeqDebug:
-    """
-    Data collator that will dynamically pad the inputs received, as well as the labels.
-
-    Args:
-        tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
-            The tokenizer used for encoding the data.
-        model ([`PreTrainedModel`]):
-            The model that is being trained. If set and has the *prepare_decoder_input_ids_from_labels*, use it to
-            prepare the *decoder_input_ids*
-
-            This is useful when using *label_smoothing* to avoid calculating loss twice.
-        padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `True`):
-            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
-            among:
-
-            - `True` or `'longest'` (default): Pad to the longest sequence in the batch (or no padding if only a single
-              sequence is provided).
-            - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
-              acceptable input length for the model if that argument is not provided.
-            - `False` or `'do_not_pad'`: No padding (i.e., can output a batch with sequences of different lengths).
-        max_length (`int`, *optional*):
-            Maximum length of the returned list and optionally padding length (see above).
-        pad_to_multiple_of (`int`, *optional*):
-            If set will pad the sequence to a multiple of the provided value.
-
-            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
-            7.5 (Volta).
-        label_pad_token_id (`int`, *optional*, defaults to -100):
-            The id to use when padding the labels (-100 will be automatically ignored by PyTorch loss functions).
-        return_tensors (`str`):
-            The type of Tensor to return. Allowable values are "np", "pt" and "tf".
-    """
-
-    tokenizer: PreTrainedTokenizerBase
-    model: Optional[Any] = None
-    padding: Union[bool, str, PaddingStrategy] = True
-    max_length: Optional[int] = None
-    pad_to_multiple_of: Optional[int] = None
-    label_pad_token_id: int = -100
-    return_tensors: str = "pt"
-
-    def __call__(self, features, return_tensors=None):
-        # features = copy.deepcopy(features)
-        if return_tensors is None:
-            return_tensors = self.return_tensors
-        labels = [feature["labels"] for feature in features] if "labels" in features[0].keys() else None
-        # We have to pad the labels before calling `tokenizer.pad` as this method won't pad them and needs them of the
-        # same length to return tensors.
-
-        print('?'*50)
-        for v in features:
-            print(len(v['input_ids']), len(v['labels']))            
-            if len(v['input_ids']) != len(v['labels']):            
-                print(self.tokenizer.decode(v['input_ids']))
-                print('-')                
-                print(self.tokenizer.decode(v['labels']))
-        print('?'*50)
-        print('====000')
-
-        if labels is not None:
-            max_label_length = max(len(l) for l in labels)
-            if self.pad_to_multiple_of is not None:
-                max_label_length = (
-                    (max_label_length + self.pad_to_multiple_of - 1)
-                    // self.pad_to_multiple_of
-                    * self.pad_to_multiple_of
-                )
-
-            padding_side = self.tokenizer.padding_side
-            for feature in features:
-                remainder = [self.label_pad_token_id] * (max_label_length - len(feature["labels"]))
-                if isinstance(feature["labels"], list):
-                    feature["labels"] = (
-                        feature["labels"] + remainder if padding_side == "right" else remainder + feature["labels"]
-                    )
-                elif padding_side == "right":
-                    feature["labels"] = np.concatenate([feature["labels"], remainder]).astype(np.int64)
-                else:
-                    feature["labels"] = np.concatenate([remainder, feature["labels"]]).astype(np.int64)
-
-        print('-')
-        for v in features:
-            print(len(v['input_ids']), len(v['labels']))
-        print('====111')
-        features = self.tokenizer.pad(
-            features,
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors=return_tensors,
-        )
-
-        #for x, y in zip(features['input_ids'], features['labels']):            
-        #    print('input_ids:', len(x), 'lables: ', len(y))
-        #print('-'*5)
-        #print('batch input_ids, labels: ', len(features['input_ids']), len(features['labels']))        
-
-        # prepare decoder_input_ids
-        if (
-            labels is not None
-            and self.model is not None
-            and hasattr(self.model, "prepare_decoder_input_ids_from_labels")
-        ):
-            decoder_input_ids = self.model.prepare_decoder_input_ids_from_labels(labels=features["labels"])
-            features["decoder_input_ids"] = decoder_input_ids
-            print('decoder_input_ids: ', len(feature['decoder_input_ids']))
-
-        print('ids, labels: ', features['input_ids'].shape, features['labels'].shape)
-        print('-'*20)
-        return features
-
-
 def train(
     # model/data params
     base_model: str = "",  # the only required argument
@@ -315,19 +201,19 @@ def train(
 
         return result
 
-    def generate_and_tokenize_prompt(data_point, eos_token):
+    def generate_and_tokenize_prompt(data_point):
         data = []
         prefix = "以下はユーザーとアシスタントの会話です。アシスタントは親切で丁寧に詳細を回答します。\n\n"
         for conversations in data_point["conversations"]:
             for i in range(len(conversations)):
                 prompt = prefix
                 for j, v in enumerate(conversations[:i+1]):
-                    prompt += "### ユーザー: \n" + v["S"] + '\n\n' + "### アシスタント: \n" + v["U"] + eos_token
+                    prompt += "### ユーザー: \n" + v["S"] + '\n\n' + "### アシスタント: \n" + v["U"]
                     if j != i:
                         prompt += '\n\n'
                     #print(prompt)
                     # print('-'*20)
-                    tokenized_prompt = tokenize(prompt)
+                    tokenized_prompt = tokenize(prompt, add_eos_token=True)
                     data.append(tokenized_prompt)
         return data
     
@@ -377,10 +263,11 @@ def train(
             test_size=val_set_size, shuffle=True, seed=42
         )
         # print('train_val', train_val)
-        train_data = generate_and_tokenize_prompt(train_val["train"].shuffle(), tokenizer.eos_token)
-        print("train_data", len(train_data))
-        val_data = generate_and_tokenize_prompt(train_val["test"].shuffle(), tokenizer.eos_token)
+        train_data = generate_and_tokenize_prompt(train_val["train"].shuffle())
+        print("train_data len", len(train_data))        
+        val_data = generate_and_tokenize_prompt(train_val["test"].shuffle())
         print("val_data", len(val_data))
+        print("train_data", train_data[0])
     else:
         train_data = data["train"].shuffle().map(generate_and_tokenize_prompt)
         val_data = None
